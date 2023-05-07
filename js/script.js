0aed6dacdd9c66dff2d60eb844cf3d0cccb11d80
Chandra Prakash <codewithcp@gmail.com>
2023-05-07 16:07:20 +0530

    Release Version 2.0.0

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+function experince_show(){
+    document.getElementById('exp').style='display:block;';
+    document.getElementById('edu').style='display:none;';
+}
+function education_show(){
+    document.getElementById('exp').style='display:none;';
+    document.getElementById('edu').style='display:block;';
+}
